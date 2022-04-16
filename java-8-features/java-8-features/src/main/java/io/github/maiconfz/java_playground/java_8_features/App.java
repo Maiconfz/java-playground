6cd4b3676b972e93e6607f1942cdba7103d90bb6
maiconfz <maiconfz@gmail.com>
2022-04-16 01:17:16 +0100

    Add example of using method references on streams

--- java-8-features/java-8-features/src/main/java/io/github/maiconfz/java_playground/java_8_features/App.java
+++ java-8-features/java-8-features/src/main/java/io/github/maiconfz/java_playground/java_8_features/App.java
@@ -1,5 +1,11 @@
 package io.github.maiconfz.java_playground.java_8_features;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
 import io.github.maiconfz.java_playground.java_8_features.interface_static_and_default_methods.Animal;
 import io.github.maiconfz.java_playground.java_8_features.interface_static_and_default_methods.Dog;
 import io.github.maiconfz.java_playground.java_8_features.interface_static_and_default_methods.UnknownAnimal;
@@ -7,6 +13,9 @@ import io.github.maiconfz.java_playground.java_8_features.interface_static_and_d
 public class App {
 	public static void main(String[] args) {
 		interfaceStaticAndDefaultMethods();
+		System.out.println("---");
+		methodReferences();
+		System.out.println("---");
 	}
 
 	public static void interfaceStaticAndDefaultMethods() {
@@ -18,5 +27,24 @@ public class App {
 		// It will call interface default method, since UnknownAnimal doesn't implement
 		// anything
 		System.out.println(b.getType());
+	}
+
+	public static void methodReferences() {
+		final List<String> stringsWithBlankStringAmongThem = Arrays.asList("Test 1", "", "Test 2");
+
+		// static method reference
+		boolean hasAnyBlankyStringOnListByStaticMethodReferece = stringsWithBlankStringAmongThem.stream().anyMatch(StringUtils::isBlank);
+		System.out.println("has any blank string on list by static method reference? " + hasAnyBlankyStringOnListByStaticMethodReferece);
+
+		// object method reference
+		boolean hasAnyEmptyStringOnListByInstanceMethodReference = stringsWithBlankStringAmongThem.stream().anyMatch(String::isEmpty);
+		System.out.println("has any empty string on list by instance method reference? " + hasAnyEmptyStringOnListByInstanceMethodReference);
+
+		long countEqualsStringsByOtherObjectMethodReferece = stringsWithBlankStringAmongThem.stream().filter(""::equals).count();
+		System.out.println("count empty strings by other object method reference: " + countEqualsStringsByOtherObjectMethodReferece);
+
+		// constructor reference
+		final List<String> listCopyByConstructorReferecen = stringsWithBlankStringAmongThem.stream().map(String::new).collect(Collectors.toList());
+		System.out.println("copied list using constructor reference: " + listCopyByConstructorReferecen);
 	}
 }
