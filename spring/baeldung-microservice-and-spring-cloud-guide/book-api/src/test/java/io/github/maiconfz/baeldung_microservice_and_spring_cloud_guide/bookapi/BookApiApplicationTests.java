b276bb205fc57af2d20af8f268ec1de15849d7dc
maiconfz <maiconfz@gmail.com>
2022-05-23 00:56:56 +0100

    Add initial spring projet

---
+++ spring/baeldung-microservice-and-spring-cloud-guide/book-api/src/test/java/io/github/maiconfz/baeldung_microservice_and_spring_cloud_guide/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.baeldung_microservice_and_spring_cloud_guide.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
