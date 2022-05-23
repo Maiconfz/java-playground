6a75ed3519ee4ed1ffb8832f3d23e4189dace852
maiconfz <maiconfz@gmail.com>
2022-05-23 01:02:09 +0100

    Init book api with spring initializer projet

---
+++ spring/baeldung-microservice-and-spring-cloud-guide/api/book/src/test/java/io/github/maiconfz/baeldung_microservice_and_spring_cloud_guide/api/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.baeldung_microservice_and_spring_cloud_guide.api.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
