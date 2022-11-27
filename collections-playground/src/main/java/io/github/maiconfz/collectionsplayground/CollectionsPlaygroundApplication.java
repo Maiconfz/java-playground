86c71a00fb7a07da250f5d1a6047a37470acebba
maiconfz <maiconfz@gmail.com>
2022-11-27 14:13:51 +0000

    Add generated spring app

---
+++ collections-playground/src/main/java/io/github/maiconfz/collectionsplayground/CollectionsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.collectionsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ collections-playground/src/test/java/io/github/maiconfz/collectionsplayground/CollectionsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.collectionsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
